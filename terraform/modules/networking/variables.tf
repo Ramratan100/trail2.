0c25f466df58cb17c7186fb3e8a43401a80f8b5f
Ram ratan yadav <rratan100@gmail.com>
2025-01-11 18:47:10 +0530

    Update variables.tf

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -33,3 +33,48 @@ variable "master_vpc_cidr" {
   description = "master cidr"
   type        = string
 }
+
+variable "Database_VPC_name" {
+  default     = "Database-VPC"
+  type        = string
+}
+
+variable "Database_IGW_name" {
+  default     = "Database-IGW"
+  type        = string
+}
+
+variable "Public_Subnet_Web_name" {
+  default     = "Public-Subnet-Web"
+  type        = string
+}
+
+variable "Public_Route_Table_name" {
+  default     = "Public-Route-Table"
+  type        = string
+}
+
+variable "Private_Subnet_Database_name" {
+  default     = "Private-Subnet-Database"
+  type        = string
+}
+
+variable "NAT_Gateway_EIP_name" {
+  default     = "NAT-Gateway-EIP"
+  type        = string
+}
+
+variable "NAT_Gateway_name" {
+  default     = "NAT-Gateway"
+  type        = string
+}
+
+variable "Private_Route_Table_Database_name" {
+  default     = "Private-Route-Table-Database"
+  type        = string
+}
+
+variable "Master_Database_VPC_Peering_name" {
+  default     = "Master-Database-VPC-Peering"
+  type        = string
+}
